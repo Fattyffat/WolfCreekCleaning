32c547d96f0de49ceabf6cbd4d100e681c0d97bf
Edvin <edvin.sinko@ucalgary.ca>
2024-10-15 14:53:13 -0600

    Lots of changes. Added pages, CSS, some text. Clean-up admin

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Wolfcreek Cleaning",
+    "email": "wolfcreekcalgary@gmail.com",
+    "phoneForTel": "403-605-3929",
+    "phoneFormatted": "(403) 605-3929",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Calgary",
+        "state": "AB",
+        "zip": "",
+        "country": "CA",
+        "mapLink": ""
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
